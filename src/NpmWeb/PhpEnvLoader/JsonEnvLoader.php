25ce2c69b36438b0436e85fc31ef7f2599576633
Josh Justice <josh@need-bee.com>
2015-07-08 12:44:53 -0400

    fixes incorrect conditional

--- src/NpmWeb/PhpEnvLoader/JsonEnvLoader.php
+++ src/NpmWeb/PhpEnvLoader/JsonEnvLoader.php
@@ -11,7 +11,7 @@ class JsonEnvLoader implements EnvLoader {
     public function load() {
         if( file_exists($this->jsonFile) ) {
             $envFromJson = (array)json_decode(file_get_contents($this->jsonFile));
-            if( !empty($envFromJson) ) {
+            if( empty($envFromJson) ) {
                 throw new \Exception("syntax error in env json file, or no config values specified");
             }
             foreach( $envFromJson as $key => $value ) {
